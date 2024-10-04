97563ef887212c0f7f9016917f1bf84d04a220f7
Maggie Boyd <maggie.boyd@stu.ocsb.ca>
2024-10-04 11:15:26 -0400

    first consolelog

--- main.js
+++ main.js
@@ -7,4 +7,12 @@
 
 
 // Feel free to delete this line when starting the task
-console.log("Hello world!")
+console.log("Apple Inc.")
+console.log("One Apple Park Way")
+console.log("Cupertino,CA")
+console.log("95014")
+console.log("+1 408-996-1010")
+console.log("\n\n")
+console.log("Microsoft Corporation\nOne Microsoft Way\nRedmond, WA\n98052\n+1 425-882-8080")
+console.log("\n\n")
+console.log("x\to\tx\no\tx\to\nx\to\tx")
